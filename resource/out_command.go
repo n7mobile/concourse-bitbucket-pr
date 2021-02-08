ed97ddf1260c0c57cdf31fdadeb8389b7108e11a
Hubert Andrzejewski <antrof@gmail.com>
2021-02-08 20:37:44 +0100

    Out step with setting commit status

--- resource/out_command.go
+++ resource/out_command.go
@@ -1,44 +1,89 @@
 package resource
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"path/filepath"
 
+	git "github.com/libgit2/git2go/v31"
+	"github.com/n7mobile/ci-bitbucket-pr/bitbucket"
+	"github.com/n7mobile/ci-bitbucket-pr/concourse"
 	"github.com/n7mobile/ci-bitbucket-pr/resource/models"
 )
 
+// OutCommand sets PullRequest status and metadata to passed one in OutRequest struct
 type OutCommand struct {
-	Logger Logger
+	Logger *concourse.Logger
 }
 
-func (cmd *OutCommand) Run(req models.OutRequest, destination string) (models.OutResponse, error) {
-	cmd.Logger.Debugf("\tReading version from %s", req.Params.VersionPath)
-
-	version, err := cmd.readVersion(destination, req.Params.VersionPath)
+// Run OutCommand processing.
+// Full SHA1 is fetched from HEAD of previous checkout step
+func (cmd *OutCommand) Run(req models.OutRequest, destination string) (*models.OutResponse, error) {
+	err := req.Source.Validate()
 	if err != nil {
-		return models.OutResponse{}, fmt.Errorf("readVersion: %w", err)
+		return nil, fmt.Errorf("resource/out: source invalid: %w", err)
 	}
 
-	return models.OutResponse{Version: *version}, nil
-}
-
-func (cmd *OutCommand) readVersion(destination, versionPath string) (*models.Version, error) {
-	path := fmt.Sprintf("%s/%s", destination, versionPath)
-
-	cmd.Logger.Debugf("\tReading from path '%s' version data", path)
-
-	bytes, err := ioutil.ReadFile(path)
+	err = req.Params.Validate()
 	if err != nil {
-		return nil, fmt.Errorf("ReadFile: %w", err)
+		return nil, fmt.Errorf("resource/out: params invalid: %w", err)
 	}
+
+	cmd.Logger.Debugf("resource/out: reading version from file %s", req.Params.VersionPath)
 
 	var version models.Version
 
-	err = json.Unmarshal(bytes, &version)
+	err = concourse.NewStorage(destination, req.Params.VersionFilename).Read(&version)
 	if err != nil {
-		return nil, fmt.Errorf("Unmarshal: %w", err)
+		return nil, fmt.Errorf("resource/out: version read: %w", err)
 	}
 
-	return &version, nil
+	cmd.Logger.Debugf("resource/out: version with commit %s", version.Commit)
+
+	dir, _ := filepath.Split(req.Params.VersionPath)
+	path := filepath.Join(destination, dir)
+
+	hash, err := cmd.gitGetHeadHash(path)
+	if err != nil {
+		return nil, fmt.Errorf("resource/out: get git head: %w", err)
+	}
+
+	cmd.Logger.Debugf("resource/out: got commit SHA1: %s", hash)
+
+	auth := bitbucket.Auth{
+		Username: req.Source.Username,
+		Password: req.Source.Password,
+	}
+
+	client := bitbucket.NewClient(req.Source.Workspace, req.Source.Slug, &auth)
+
+	statReq := bitbucket.CommitBuildStatusRequest{
+		Key:         "build",
+		Name:        req.Params.Name,
+		Description: req.Params.Description,
+		URL:         req.Params.URL,
+		State:       bitbucket.CommitBuildStatus(req.Params.Status),
+	}
+
+	cmd.Logger.Debugf("Setting commit %s build status to %s", hash, statReq.State)
+
+	err = client.SetCommitBuildStatus(version.Commit, &statReq)
+	if err != nil {
+		return nil, fmt.Errorf("SetCommitBuildStatus: %w", err)
+	}
+
+	return &models.OutResponse{Version: version}, nil
+}
+
+func (cmd *OutCommand) gitGetHeadHash(path string) (string, error) {
+	repo, err := git.OpenRepository(path)
+	if err != nil {
+		return "", fmt.Errorf("resource/out: open repo at path %s: %w", path, err)
+	}
+
+	head, err := repo.Head()
+	if err != nil {
+		return "", fmt.Errorf("resource/out: get head: %w", err)
+	}
+
+	return head.Target().String(), nil
 }
